d1d592c791799ed62e6fe784c4579e25867f03c1
ar0177417 <ar0177417@gmail.com>
2021-12-28 16:01:48 +0500

    Wrote Basic Install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
-#Boot into installaionmedium install and mount the installtion drive
+#!/bin/sh
+# Boot into installaion medium install and mount the installtion drive
+# fdisk for partitioning
+# mkfs.ext4 /dev/sdax to format drive
 # e.g mount /dev/sda1 /mnt
-# execute pacstrap 
-# pacstrap /mnt
+# Execute pacstrap
+# pacstrap /mnt base linux linux-firmware
+# arch-chroot /mnt
+# genfstab -U /mnt >> /mnt/etc/fstab
 # In Arch-Chroot execute this
-pacman -Syu && pacman -S nmcli
-
+pacman -Syu && pacman -S nmcli grub efibootmgr
+nmcli dev wifi
+read -e -i "nmcli dev wifi connect " && eval "$REPLY"
+ln -sf /usr/share/zoneinfo/Asia/Karachi /etc/localtime
+hwclock --systohc
+read -e -i "echo '' >> /etc/hostname" && eval "$REPLY"
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8 UTF-8" >> /etc/locale.conf
+mkinitcpio -P
+passwd
+#For BIOS Systems
+read -e -i "grub-install --target=i386-pc /dev/sda" && eval "$REPLY"
+read -e -i "grub-mkconfig -o /boot/grub/grub.cfg" && eval "$REPLY"
