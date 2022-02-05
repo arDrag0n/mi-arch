0b73d22a38a7ec71e6a8894e3c6b4104d47882af
ar0177417 <ar0177417@gmail.com>
2022-02-05 10:28:39 +0500

    Added root.sh

--- root.sh
+++ root.sh
@@ -1,4 +1,5 @@
-pacman -Sy && pacman -S git base-devel
+pacman -Syu
+pacman -S sudo
+pacman -S --needed git base-devel
 cd /opt && git clone https://aur.archlinux.org/yay-git.git && cd yay-git
 makepkg -si
-
